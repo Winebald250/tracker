08cca6073e750cdb489849616f29ac95b6a9fef3
Winebald250 <winebaldbanituze@gmail.com>
2024-10-07 20:52:38 +0200

    DET

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Expenditure</title>
+    <title>DET</title>
 
     <!-- ==== STYLE.CSS ==== -->
     <!-- <link href="css/style.css" rel="stylesheet" /> -->
