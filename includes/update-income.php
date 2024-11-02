b66026c430fb2bd7c39e324d57859490dac80da7
Winebald250 <winebaldbanituze@gmail.com>
2024-11-02 22:19:53 +0200

    DET

--- includes/update-income.php
+++ includes/update-income.php
@@ -15,27 +15,27 @@ if (isset($_POST['expense-id'])) {
   
   // Retrieve the updated expense details from the form data
   $name = isset($_POST['name']) ? $_POST['name'] : '';
-  $date = isset($_POST['date_of_lending']) ? $_POST['date_of_lending'] : '';
+  $date = isset($_POST['date_of_income']) ? $_POST['date_of_income'] : '';
   $description = isset($_POST['description']) ? $_POST['description'] : '';
   $amount = isset($_POST['amount']) ? $_POST['amount'] : '';
   $status = isset($_POST['status']) ? $_POST['status'] : '';
 
   // Prepare and execute the SQL query to update the expense in the database
-  $stmt = $db->prepare('UPDATE lending SET name = ?, date_of_lending = ?, description = ?, amount = ?, status = ? WHERE id = ? AND UserId = ?');
+  $stmt = $db->prepare('UPDATE income SET name = ?, date_of_income = ?, description = ?, amount = ?, status = ? WHERE id = ? AND UserId = ?');
   $stmt->bind_param('ssssssi', $name, $date, $description, $amount, $status, $id, $userid);
   $result = $stmt->execute();
 
   // Check if the update was successful and redirect to the expenses page with a success message
   if ($result) {
-    $message = "Lending updated successfully";
+    $message = "income updated successfully";
     echo "<script type='text/javascript'>alert('$message');</script>";
-    echo " <script type='text/javascript'>window.location.href = 'manage-lending.php';</script>";
+    echo " <script type='text/javascript'>window.location.href = 'manage-income.php';</script>";
     exit();
   } else {
     // If the update failed, redirect to the edit expense page with an error message
     $message = "Please try again !";
     echo "<script type='text/javascript'>alert('$message');</script>";
-    echo " <script type='text/javascript'>window.location.href = 'manage-lending.php?id=' . $id';</script>";
+    echo " <script type='text/javascript'>window.location.href = 'manage-income.php?id=' . $id';</script>";
     exit();
   }
 }
